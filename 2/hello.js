6f6a4f5a1024588e44e6be172ed903e146d01f34
chencl2bjtest2 <140798006+chencl2bjtest2@users.noreply.github.com>
2023-08-08 16:42:01 +0800

    Delete 2 directory

--- 2/hello.js
+++
@@ -1,9 +0,0 @@
-// @see https://docs.aircode.io/guide/functions/
-const aircode = require('aircode');
-
-module.exports = async function (params, context) {
-  console.log('Received params:', params);
-  return {
-    message: 'Hi, AirCode.',
-  };
-};
